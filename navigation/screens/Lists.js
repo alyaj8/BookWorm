4c38ce02cf9a1220f9218738007d0b3aa9f0d255
alyaj8 <99920164+alyaj8@users.noreply.github.com>
2022-09-08 00:23:35 +0300

    h

---
+++ navigation/screens/Lists.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Lists({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text
+             //   onPress={() => alert('This is the "lists" screen.')}///navigation.navigate('home')
+                style={{ fontSize: 26, fontWeight: 'bold' }}>Lists</Text>
+        </View>
+    );
+}
---
+++ navigation/screens/More.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+export default function More({ navigation }) {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <Text
+              //  onPress={() => alert('This is the "more" screen.')}
+                style={{ fontSize: 26, fontWeight: 'bold' }}>More 3 </Text>
+        </View>
+    );
+}
