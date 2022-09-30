692d0c0eb14b870abd066225df224591d7c92d61
Mashaelaltheeb <112610017+Mashaelaltheeb@users.noreply.github.com>
2022-09-30 13:17:02 +0300

    AddBookTest Manually

--- navigation/screens/AddBookTest.js
+++ navigation/screens/AddBookTest.js
@@ -14,36 +14,28 @@ import {
   TouchableHighlight,
   TouchableOpacity,
   ScrollView,
+  Keyboard,
 } from "react-native";
 import { Entypo } from "@expo/vector-icons";
 import { Formik } from "formik";
 import * as ImagePicker from "expo-image-picker";
-import { getAuth, createUserWithEmailAndPassword} from "firebase/auth";
-import {collection, doc, getFirestore, setDoc} from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  collection,
+  doc,
+  getFirestore,
+  setDoc,
+  firestore,
+  addDoc,
+} from "firebase/firestore";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
+import Toast, { BaseToast, ErrorToast } from "react-native-toast-message";
+
 import { AntDesign } from "@expo/vector-icons";
-const options = {
-    title: "select image",
-    type: "library",
-    options: {
-      maxHeight: 200,
-      maxWidth: 200,
-      selectionLimit: 1,
-      mediaType: "photo",
-      includeBase64: false,
-    },
-  };
-  const pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync(options);
 
-    if (!result.cancelled) {
-      setImage(result.uri);
-    }
-  };
-  //remove image
-  const removeImage = () => setImage(null);
+import background_image from "./222.jpg";
 
-  const background_image = { uri: ".222.jpg" };
-function msg (error){
+/*function msg (error){
   switch (error.code){
          case "auth/invalid-email":
           error.code = "Wrong email address";
@@ -62,21 +54,199 @@ function msg (error){
           return error.code;
         }
         return error.code;
-}
+}*/
 
 export default function AddBookTest({ navigation }) {
-const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null);
   const [value, setValue] = React.useState({
     title: "",
     Description: "",
-    category:"",
-    ISBN:"",
-    author:"",
-    poster:"",
+    category: "",
+    ISBN: "",
+    author: "",
+    poster: "",
+    virsion: "",
     error: "",
   });
-  const auth = getAuth();
-  async function addBook() {
+  const [Error, setError] = useState({
+    title: true,
+    Description: true,
+    category: true,
+    ISBN: true,
+    author: true,
+    poster: true,
+    virsion: true,
+  });
+
+  /// AddImage
+  const options = {
+    title: "select image",
+    type: "library",
+    options: {
+      maxHeight: 200,
+      maxWidth: 200,
+      selectionLimit: 1,
+      mediaType: "photo",
+      includeBase64: false,
+    },
+  };
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync(options);
+
+    if (!result.cancelled) {
+      // firebase;
+      var name = Math.random();
+      const storage = getStorage();
+      const storageRef = ref(storage, `posters${name}`);
+      const response = await fetch(result.uri);
+      const file = await response.blob();
+      uploadBytes(storageRef, file).then((snapshot) => {
+        console.log(snapshot);
+        getDownloadURL(snapshot.ref).then((url) => {
+          setImage(url);
+        });
+        console.log("Uploaded a blob or file!");
+      });
+    }
+  };
+  //remove image
+  const removeImage = () => setImage(null);
+
+  // const auth = getAuth();
+
+  ///////////////////////////////new code
+  const [addDate, setAddData] = useState("");
+
+  //add a new data
+  async function addField() {
+    if (value.title === "") {
+      setError({
+        ...Error,
+        title: false,
+        virsion: true,
+        author: true,
+        ISBN: true,
+        category: true,
+        Description: true,
+        title: true,
+      });
+      return;
+    }
+
+    if (value.Description === "") {
+      setError({
+        ...Error,
+        Description: false,
+        title: true,
+        virsion: true,
+        author: true,
+        ISBN: true,
+        category: true,
+      });
+      return;
+    }
+    if (value.category === "") {
+      setError({
+        ...Error,
+        Description: true,
+        title: true,
+        virsion: true,
+        author: true,
+        ISBN: true,
+        category: false,
+      });
+      return;
+    }
+    if (value.ISBN === "") {
+      setError({
+        ...Error,
+        Description: true,
+        title: true,
+        virsion: true,
+        author: true,
+        ISBN: false,
+        category: true,
+      });
+      return;
+    }
+    if (value.author === "") {
+      setError({
+        ...Error,
+        Description: true,
+        title: true,
+        virsion: true,
+        author: false,
+        ISBN: true,
+        category: true,
+      });
+      return;
+    }
+
+    if (value.virsion === "") {
+      setError({
+        ...Error,
+        virsion: false,
+        author: true,
+        ISBN: true,
+        category: true,
+        Description: true,
+        title: true,
+      });
+      return;
+    }
+
+    if (image === "") {
+      setError({
+        ...Error,
+        poster: false,
+      });
+      return;
+    }
+
+    try {
+      const db = getFirestore();
+      // check if we have new feilds data
+      //get the timestamp
+      const data = {
+        title: value.title,
+        Description: value.Description,
+        category: value.category,
+        ISBN: value.ISBN,
+        author: value.author,
+        poster: image,
+      };
+      await addDoc(collection(db, "Book"), data);
+      showToast();
+      setError({
+        ...Error,
+        virsion: true,
+        author: true,
+        ISBN: true,
+        category: true,
+        Description: true,
+        title: true,
+      });
+      setValue({
+        title: "",
+        Description: "",
+        category: "",
+        ISBN: "",
+        author: "",
+        poster: "",
+        virsion: "",
+        error: "",
+      });
+      setImage(null);
+    } catch (error) {
+      setValue({
+        ...value,
+        error: error,
+      });
+      console.log(error);
+    }
+  }
+  ////////////////////////////////////end new code
+  /*async function addBook() {
 
     if ( value.title ==="" || value.Description === "" || value.category === ""||value.author === ""||value.ISBN === "" ){
       setValue({
@@ -109,39 +279,85 @@ const [image, setImage] = useState(null);
       });
       console.log(er);
     }
-  }
-  
-  return (
-    <ImageBackground source={background_image} resizeMode="cover" >
-    <ScrollView>
-    <Image
-            style={{ height: "100%", width: "100%" , position:"absolute" }}
-            source={require("../screens/222.jpg")}
-          />
-    <View style={styles.container}>
-    
-      <View
-        style={{
-          width: "100%",
-          height: 40,
-          paddingHorizontal: 20,
-        }}
-      >
-        <Text style={{ fontSize: 22 }} onPress={() => navigation.goBack()}>
-          Back
-        </Text>
-      </View>
-      <Text style={[styles.title, styles.leftTitle]}>Add new Book</Text>
-      <Text style={{ color: "red" }}>{value?.error}</Text>
+  }*/
 
-      <View style={styles.InputContainer}>
-      <View style={{alignItems:'center'}}>
+  const toastConfig = {
+    success: (props) => (
+      <BaseToast
+        {...props}
+        style={{ borderLeftColor: "green" }}
+        contentContainerStyle={{
+          paddingHorizontal: 15,
+        }}
+        text1Style={{
+          fontSize: 15,
+          fontWeight: "400",
+        }}
+      />
+    ),
+    /*
+      Overwrite 'error' type,
+      by modifying the existing `ErrorToast` component
+    */
+  };
+
+  const showToast = () => {
+    Toast.show({
+      type: "success",
+      text1: "Success",
+      text2: "Book Successfully Added",
+      position: "bottom",
+      // And I can pass any custom props I want
+      props: { uuid: "bba1a7d0-6ab2-4a0a-a76e-ebbe05ae6d70" },
+    });
+  };
+
+  // showToast();
+
+  return (
+    <ImageBackground source={background_image} resizeMode="cover">
+      <ScrollView>
+        <Image
+          style={{ height: "100%", width: "100%", position: "absolute" }}
+          source={require("../screens/222.jpg")}
+        />
+
+        <View style={styles.container}>
+          <View
+            style={{
+              width: "100%",
+              height: 40,
+              paddingHorizontal: 20,
+            }}
+          >
+            <Text style={{ fontSize: 22 }} onPress={() => navigation.goBack()}>
+              Back
+            </Text>
+          </View>
+          <Text style={[styles.title, styles.leftTitle]}>Add new Book</Text>
+          <Text
+            style={{
+              color: "red",
+              textAlign: "center",
+              fontSize: 15,
+              fontWeight: "bold",
+            }}
+          >
+            {value?.error}
+          </Text>
+
+          <View style={styles.InputContainer}>
+            <View style={{ alignItems: "center" }}>
               <TouchableHighlight onPress={() => pickImage()}>
-                <View style={{alignItems:"center"}} 
-                onChangeText={(text) => setValue({ ...value, poster: URL })}>
+                <View
+                  style={{ alignItems: "center" }}
+                  onChangeText={(text) => setValue({ ...value, poster: URL })}
+                >
                   <Entypo name="images" size={24} color="#576F72" />
-                  
-                  <Text style={{ alignContent:'center'}} >the book's poster</Text>
+
+                  <Text style={{ alignContent: "center" }}>
+                    the book's poster
+                  </Text>
                 </View>
               </TouchableHighlight>
 
@@ -151,8 +367,7 @@ const [image, setImage] = useState(null);
                     display: "flex",
                     flexDirection: "row",
                     marginRight: 5,
-                    alignContent:'center',
-                    
+                    alignContent: "center",
                   }}
                 >
                   <Image
@@ -169,81 +384,155 @@ const [image, setImage] = useState(null);
               )}
             </View>
 
-        <Text style={styles.textD} s>Title</Text>
-        <TextInput
-          style={styles.body}
-          //placeholder="First Name"
-          onChangeText={(text) => setValue({ ...value, title: text })}
-          underlineColorAndroid="transparent"
-        />
+            <Text style={styles.textD}>Title</Text>
+            {!Error.title && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 10,
+                }}
+              >
+                This Field is mandatory
+              </Text>
+            )}
+            <TextInput
+              style={styles.body}
+              //placeholder="First Name"
+              onChangeText={(text) => setValue({ ...value, title: text })}
+              underlineColorAndroid="transparent"
+              value={value.title}
+            />
+          </View>
+          <View style={styles.InputContainer}>
+            <Text style={styles.textD}>Description</Text>
 
-      </View>
-      <View style={styles.InputContainer}>
-        <Text style={styles.textD}>Description</Text>
-        <TextInput
-          multiline = {true}
-          
-          style={styles.body}
-          //placeholder="Last Name"
-          onChangeText={(text) => setValue({ ...value, Description: text })}
-          underlineColorAndroid="transparent"
-        />
-      </View>
-      <View style={styles.InputContainer}>
-        <Text style={styles.textD}>Category</Text>
-        <TextInput
-          style={styles.body}
-         // placeholder="Username"
-          onChangeText={(text) => setValue({ ...value, category: text })}
-          underlineColorAndroid="transparent"
-        />
-      </View>
+            {!Error.Description && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 10,
+                }}
+              >
+                This Field is mandatory
+              </Text>
+            )}
+            <TextInput
+              multiline={true}
+              style={styles.body}
+              //placeholder="Last Name"
+              value={value.Description}
+              onChangeText={(text) => setValue({ ...value, Description: text })}
+              underlineColorAndroid="transparent"
+            />
+          </View>
+          <View style={styles.InputContainer}>
+            <Text style={styles.textD}>Category</Text>
 
-      <View></View>
+            {!Error.category && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 10,
+                }}
+              >
+                This Field is mandatory
+              </Text>
+            )}
+            <TextInput
+              style={styles.body}
+              value={value.category}
+              // placeholder="Username"
+              onChangeText={(text) => setValue({ ...value, category: text })}
+              underlineColorAndroid="transparent"
+            />
+          </View>
 
-      <View style={styles.InputContainer}>
-        <Text style={styles.textD}>ISBN</Text>
-        <TextInput
-          style={styles.body}
-          //placeholder="E-mail"
-          onChangeText={(text) => setValue({ ...value, ISBN: text })}
-          underlineColorAndroid="transparent"
-        />
-      </View>
+          <View></View>
 
-      <View style={styles.InputContainer}>
-        <Text style={styles.textD}>Author</Text>
-        <TextInput
-          style={styles.body}
-          //secureTextEntry={true}
-          //placeholder="Password"
-          onChangeText={(text) => setValue({ ...value, author: text })}
-          underlineColorAndroid="transparent"
-        />
-      </View>
-      <View>
-      <Text style={styles.textD}>the book's electronic virsion</Text>
-        <TextInput
-          style={styles.body}
-          //secureTextEntry={true}
-          //placeholder="Password"
-          onChangeText={(text) => setValue({ ...value, poster: text })}
-          underlineColorAndroid="transparent"
-        />
-      </View>
+          <View style={styles.InputContainer}>
+            <Text style={styles.textD}>ISBN</Text>
+            {!Error.ISBN && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 10,
+                }}
+              >
+                This Field is mandatory
+              </Text>
+            )}
+            <TextInput
+              style={styles.body}
+              //placeholder="E-mail"
+              value={value.ISBN}
+              onChangeText={(text) => setValue({ ...value, ISBN: text })}
+              underlineColorAndroid="transparent"
+            />
+          </View>
 
-      <View style={styles.buttonCont}>
-        <Button 
-        
-          title="Add Book"
-          color="#B1D8B7"
-          onPress={() => addBook()} //
-        ></Button>
-      </View>
-      
-    </View>
-    
-    </ScrollView>
+          <View style={styles.InputContainer}>
+            <Text style={styles.textD}>Author</Text>
+            {!Error.author && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 10,
+                }}
+              >
+                This Field is mandatory
+              </Text>
+            )}
+            <TextInput
+              style={styles.body}
+              //secureTextEntry={true}
+              //placeholder="Password"
+              value={value.author}
+              onChangeText={(text) => setValue({ ...value, author: text })}
+              underlineColorAndroid="transparent"
+            />
+          </View>
+          <View>
+            <Text style={styles.textD}>the book's electronic virsion</Text>
+            {!Error.virsion && (
+              <Text
+                style={{
+                  color: "red",
+                  marginLeft: 10,
+                }}
+              >
+                This Field is mandatory
+              </Text>
+            )}
+            <TextInput
+              style={styles.body}
+              //secureTextEntry={true}
+              //placeholder="Password"
+              value={value.virsion}
+              onChangeText={(text) => setValue({ ...value, virsion: text })}
+              underlineColorAndroid="transparent"
+            />
+          </View>
+
+          <View style={styles.buttonCont}>
+            <TouchableOpacity
+              style={{
+                width: "100%",
+                backgroundColor: "#B1D8B7",
+                height: 40,
+                borderRadius: 10,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              onPress={() => addField()}
+            >
+              <Text style={{ color: "white", fontWeight: "bold" }}>
+                ADD BUTTON
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+      <Toast config={toastConfig} />
     </ImageBackground>
   );
 }
@@ -256,7 +545,7 @@ const styles = StyleSheet.create({
   },
   title: {
     fontWeight: "bold",
-    alignSelf:'center',
+    alignSelf: "center",
     fontSize: 35,
     marginTop: 20,
     marginBottom: 20,
@@ -270,27 +559,20 @@ const styles = StyleSheet.create({
     height: 42,
     paddingLeft: 20,
     paddingRight: 20,
-    alignSelf:'center'
+    alignSelf: "center",
   },
   buttonCont: {
     margin: 20,
     padding: 5,
     width: 250,
-    alignSelf:'center',
+    alignSelf: "center",
     borderRadius: 50,
     //backgroundColor: "#B1D8B7",
   },
-  InputContainer:
-  {
+  InputContainer: {
     fontSize: 50,
-  
   },
-  textD:{
-    marginLeft:40,
-  }
+  textD: {
+    marginLeft: 40,
+  },
 });
-
-
-
-
-
