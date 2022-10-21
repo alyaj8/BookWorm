c9d56e486a8174511cc27af9b17a90e349cbf277
alyaj8 <99920164+alyaj8@users.noreply.github.com>
2022-10-22 00:03:47 +0300

    home adding templete for recommendation

--- navigation/screens/Home.js
+++ navigation/screens/Home.js
@@ -13,38 +13,32 @@ import {
   Button,
   Alert,
   ImageBackground,
+  TouchableOpacity,
 } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 //import { LinearGradient } from "expo-linear-gradient";
+import { useEffect, useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Home({ navigation }) {
+  let [rec, setrec] = useState([]);
+
   return (
     <View
       style={{
-        backgroundColor: "#EDF5F0",
+        backgroundColor: "#edf5f0",
         flex: 1,
       }}
     >
       <View
         style={{
           backgroundColor: "#00a46c",
-          height: "15%",
+          height: "14%",
           borderBottomLeftRadius: 20,
           borderBottomRightRadius: 20,
           paddingHorizontal: 20,
         }}
-      >
-        <View
-          style={{
-            flexDirection: "row",
-            alignItems: "center",
-            marginTop: 20,
-            width: "100%",
-          }}
-        >
-          
-        </View>
-      </View>
+      ></View>
       <View
         colors={["rgba(0,164,109,0.4)"]}
         style={{
@@ -57,15 +51,16 @@ export default function Home({ navigation }) {
         <View
           style={{
             backgroundColor: "#FFF",
-            paddingVertical: 8,
+            paddingVertical: 10,
             paddingHorizontal: 20,
             marginHorizontal: 20,
-            borderRadius: 15,
-            marginTop: "4.6%",
+            borderRadius: 20,
+            marginTop: "3.5%",
             flexDirection: "row",
             alignItems: "center",
             borderColor: "black",
             borderWidth: 0.2,
+            height: 46,
           }}
         >
           <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
@@ -80,6 +75,220 @@ export default function Home({ navigation }) {
             }}
           />
         </View>
+      </View>
+
+      <View
+        style={{
+          width: "100%",
+          height: "35%",
+          backgroundColor: "#f6fff9",
+          paddingHorizontal: 9,
+        }}
+      >
+        <TouchableOpacity>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 17,
+              color: "#585a61",
+              marginBottom: -18,
+              fontWeight: "bold",
+              marginTop: 8,
+            }}
+          >
+            Recommendation
+          </Text>
+          <View
+            style={{
+              backgroundColor: "#00a46c",
+              paddingHorizontal: 20,
+              paddingVertical: 5,
+              borderRadius: 15,
+              alignSelf: "flex-end",
+              width: 89,
+              height: 27,
+            }}
+          >
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: 13,
+                color: "#FFF",
+              }}
+            >
+              View all
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={true}
+          style={{ height: 300 }}
+        >
+          <LinearGradient
+            colors={["rgba(0,164,109,0.09)", "transparent"]}
+            style={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              height: 130,
+              marginTop: 170,
+              top: 0,
+              width: 9999,
+            }}
+          />
+          <TouchableOpacity
+            // key={ind}
+            //  onPress={() => OpenInfo(val)}
+            style={{
+              height: 250,
+              elevation: 2,
+              //   backgroundColor: "#FFF",
+              marginLeft: 20,
+              marginTop: 20,
+              borderRadius: 15,
+              marginBottom: 10,
+              width: 160,
+            }}
+            //  disabled={val.deleted}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                paddingTop: 10,
+                paddingHorizontal: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: "bold",
+                }}
+              ></Text>
+            </View>
+          </TouchableOpacity>
+          <Text
+            style={{
+              //  flex: 1,
+              //textAlign: "center",
+              marginTop: -10, ////////////////////////////////////////
+              marginLeft: 10, ////////////////////////////////////////
+              fontSize: 15,
+              fontWeight: "bold",
+              color: "grey",
+              alignSelf: "center",
+            }}
+          >
+            Empty
+          </Text>
+        </ScrollView>
+      </View>
+
+      <View
+        style={{
+          width: "100%",
+          height: "35%",
+          backgroundColor: "#f6fff9",
+          paddingHorizontal: 9,
+        }}
+      >
+        <TouchableOpacity>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 17,
+              color: "#585a61",
+              marginBottom: -18,
+              fontWeight: "bold",
+              marginTop: 8,
+            }}
+          >
+            Most rated
+          </Text>
+          <View
+            style={{
+              backgroundColor: "#00a46c",
+              paddingHorizontal: 20,
+              paddingVertical: 5,
+              borderRadius: 15,
+              alignSelf: "flex-end",
+              width: 89,
+              height: 27,
+            }}
+          >
+            <Text
+              style={{
+                fontWeight: "bold",
+                fontSize: 13,
+                color: "#FFF",
+              }}
+            >
+              View all
+            </Text>
+          </View>
+        </TouchableOpacity>
+
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={true}
+          style={{ height: 300 }}
+        >
+          <LinearGradient
+            colors={["rgba(0,164,109,0.09)", "transparent"]}
+            style={{
+              position: "absolute",
+              left: 0,
+              right: 0,
+              height: 150,
+              marginTop: 170,
+              top: 0,
+              width: 9999,
+            }}
+          />
+          <TouchableOpacity
+            // key={ind}
+            //  onPress={() => OpenInfo(val)}
+            style={{
+              height: 250,
+              elevation: 2,
+              //   backgroundColor: "#FFF",
+              marginLeft: 20,
+              marginTop: 20,
+              borderRadius: 15,
+              marginBottom: 10,
+              width: 160,
+            }}
+            //  disabled={val.deleted}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                paddingTop: 10,
+                paddingHorizontal: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: "bold",
+                }}
+              ></Text>
+            </View>
+          </TouchableOpacity>
+          <Text
+            style={{
+              //  flex: 1,
+              //textAlign: "center",
+              marginTop: -10, ////////////////////////////////////////
+              marginLeft: 10, ////////////////////////////////////////
+              fontSize: 15,
+              fontWeight: "bold",
+              color: "grey",
+              alignSelf: "center",
+            }}
+          >
+            Empty
+          </Text>
+        </ScrollView>
       </View>
     </View>
   );
