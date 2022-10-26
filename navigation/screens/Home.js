72a2bb6d1d063614ee5963f7ab6654639ff0ac1b
alyaj8 <99920164+alyaj8@users.noreply.github.com>
2022-10-26 21:00:10 +0300

    (notFinished)recommendation

--- navigation/screens/Home.js
+++ navigation/screens/Home.js
@@ -21,10 +21,110 @@ import Icon from "react-native-vector-icons/Ionicons";
 //import { LinearGradient } from "expo-linear-gradient";
 import { useEffect, useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
+import { getAuth } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
 
 export default function Home({ navigation }) {
   let [rec, setrec] = useState([]);
   let [modalVisible, setModalVisible] = useState(false);
+  let [RecommendationList, setRecommendationList] = useState([]);
+  let [categories, setCategories] = useState([]);
+  let [faveIds, setFavIds] = useState([]);
+
+  const Datacat = (str, num) => {
+    if (str.length > num) {
+      return str.substring(0, num) + "...";
+    }
+    return str;
+  };
+
+  let GetBookFavList = async () => {
+    try {
+      let list = [];
+      let ids = [];
+      const Auth = getAuth();
+      Auth.onAuthStateChanged(async (user) => {
+        const db = getFirestore();
+
+        const q = query(
+          collection(db, "favoriteList"),
+          where("favouriteListUserId", "==", user.uid)
+        );
+        const querySnapshot = await getDocs(q);
+
+        if (!querySnapshot.empty) {
+          querySnapshot.forEach((doc) => {
+            let book = doc.data();
+
+            let check = book.category.includes(",");
+            if (check) {
+              list.push(book.category);
+
+              let splitRarray = book.category.split(",");
+              splitRarray.map((v) => {
+                list.push(v);
+              });
+            } else {
+              list.push(book.category);
+            }
+            ids.push(book.id);
+          });
+        }
+        let uniqueChat = [...new Set(list)];
+        setFavIds(ids);
+        setCategories(uniqueChat);
+        Recommendation(uniqueChat);
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  let Recommendation = async (favoriteCatList) => {
+    try {
+      let list = [];
+      const db = getFirestore();
+      if (favoriteCatList.length > 0) {
+        for (var i = 0; i < favoriteCatList.length; i++) {
+          const q = query(
+            collection(db, "Book"),
+            where("category", "==", favoriteCatList[i])
+          );
+          const querySnapshot = await getDocs(q);
+
+          if (!querySnapshot.empty) {
+            querySnapshot.forEach((doc) => {
+              let book = doc.data();
+              book.id = doc.id;
+              console.log(doc.id, "iddid");
+              list.push(book);
+            });
+          }
+        }
+        list = list.filter(
+          (book, index, self) =>
+            index === self.findIndex((t) => t.title === book.title)
+        );
+        setRecommendationList(list);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    navigation.addListener("focus", async () => {
+      await GetBookFavList();
+    });
+  }, []);
+
+  console.log(faveIds, "========>ids");
 
   return (
     <View
@@ -79,27 +179,135 @@ export default function Home({ navigation }) {
           />
         </View>
       </View>
-      <View
-        style={{
-          width: "100%",
-          height: "35%",
-          backgroundColor: "#f6fff9",
-          paddingHorizontal: 9,
-        }}
-      >
-        <Text
+
+      <ScrollView>
+        <View
           style={{
-            fontWeight: "bold",
-            fontSize: 17,
-            color: "#585a61",
-            marginBottom: -18,
-            fontWeight: "bold",
-            marginTop: 8,
+            width: "100%",
+            height: "35%",
+            backgroundColor: "#f6fff9",
+            paddingHorizontal: 9,
           }}
         >
-          Highest Rated Books
-        </Text>
-        <TouchableOpacity>
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 17,
+              color: "#585a61",
+              marginBottom: -18,
+              fontWeight: "bold",
+              marginTop: 8,
+            }}
+          >
+            Highest Rated Books
+          </Text>
+          <TouchableOpacity>
+            <View
+              style={{
+                backgroundColor: "#00a46c",
+                paddingHorizontal: 20,
+                paddingVertical: 5,
+                borderRadius: 15,
+                alignSelf: "flex-end",
+                width: 89,
+                height: 27,
+              }}
+            >
+              <Text
+                style={{
+                  fontWeight: "bold",
+                  fontSize: 13,
+                  color: "#FFF",
+                }}
+              >
+                View all
+              </Text>
+            </View>
+          </TouchableOpacity>
+
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={true}
+            style={{ height: 300 }}
+          >
+            <LinearGradient
+              colors={["rgba(0,164,109,0.09)", "transparent"]}
+              style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                height: 150,
+                marginTop: 170,
+                top: 0,
+                width: 9999,
+              }}
+            />
+            <TouchableOpacity
+              style={{
+                height: 250,
+                elevation: 2,
+                marginLeft: 20,
+                marginTop: 20,
+                borderRadius: 15,
+                marginBottom: 10,
+                width: 160,
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  paddingTop: 10,
+                  paddingHorizontal: 10,
+                }}
+              >
+                <Text
+                  style={{
+                    fontWeight: "bold",
+                  }}
+                ></Text>
+              </View>
+            </TouchableOpacity>
+            <Text
+              style={{
+                marginTop: -10,
+                marginLeft: 10,
+                fontSize: 15,
+                fontWeight: "bold",
+                color: "grey",
+                alignSelf: "center",
+              }}
+            >
+              Empty
+            </Text>
+          </ScrollView>
+        </View>
+        <View
+          style={{
+            width: "100%",
+            height: "35%",
+            backgroundColor: "#f6fff9",
+            paddingHorizontal: 9,
+          }}
+        >
+          <Text
+            style={{
+              fontWeight: "bold",
+              fontSize: 17,
+              color: "#585a61",
+              marginBottom: -18,
+              fontWeight: "bold",
+              marginTop: 8,
+            }}
+          >
+            Recommendation
+          </Text>
+          <Icon
+            onPress={() => setModalVisible(true)}
+            name="information-circle-outline"
+            size={30}
+            style={{ color: "black", marginLeft: 230, marginBottom: -30 }}
+          />
+
           <View
             style={{
               backgroundColor: "#00a46c",
@@ -111,216 +319,145 @@ export default function Home({ navigation }) {
               height: 27,
             }}
           >
-            <Text
-              style={{
-                fontWeight: "bold",
-                fontSize: 13,
-                color: "#FFF",
-              }}
-            >
-              View all
-            </Text>
+            <Button
+              onPress={() =>
+                navigation.navigate("RecViewall", RecommendationList)
+              }
+              title="View all"
+            ></Button>
+            <Text>View all</Text>
           </View>
-        </TouchableOpacity>
-
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={true}
-          style={{ height: 300 }}
-        >
-          <LinearGradient
-            colors={["rgba(0,164,109,0.09)", "transparent"]}
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              height: 150,
-              marginTop: 170,
-              top: 0,
-              width: 9999,
-            }}
-          />
-          <TouchableOpacity
-            // key={ind}
-            //  onPress={() => OpenInfo(val)}
-            style={{
-              height: 250,
-              elevation: 2,
-              //   backgroundColor: "#FFF",
-              marginLeft: 20,
-              marginTop: 20,
-              borderRadius: 15,
-              marginBottom: 10,
-              width: 160,
-            }}
-            //  disabled={val.deleted}
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible}
           >
-            <View
-              style={{
-                flexDirection: "row",
-                paddingTop: 10,
-                paddingHorizontal: 10,
-              }}
-            >
-              <Text
+            <View>
+              <View
                 style={{
-                  fontWeight: "bold",
+                  backgroundColor: "white",
+                  margin: 50,
+                  padding: 50,
+                  marginTop: 400,
+                  borderRadius: 5,
+                  elevation: 5,
                 }}
-              ></Text>
-            </View>
-          </TouchableOpacity>
-          <Text
-            style={{
-              //  flex: 1,
-              //textAlign: "center",
-              marginTop: -10, ////////////////////////////////////////
-              marginLeft: 10, ////////////////////////////////////////
-              fontSize: 15,
-              fontWeight: "bold",
-              color: "grey",
-              alignSelf: "center",
-            }}
-          >
-            Empty
-          </Text>
-        </ScrollView>
-      </View>
-      <View
-        style={{
-          width: "100%",
-          height: "35%",
-          backgroundColor: "#f6fff9",
-          paddingHorizontal: 9,
-        }}
-      >
-        <Text
-          style={{
-            fontWeight: "bold",
-            fontSize: 17,
-            color: "#585a61",
-            marginBottom: -18,
-            fontWeight: "bold",
-            marginTop: 8,
-          }}
-        >
-          Recommendation
-        </Text>
-        <Icon
-          onPress={() => setModalVisible(true)}
-          name="information-circle-outline"
-          size={30}
-          style={{ color: "black", marginLeft: 230, marginBottom: -30 }}
-        />
-
-        <View
-          style={{
-            backgroundColor: "#00a46c",
-            paddingHorizontal: 20,
-            paddingVertical: 5,
-            borderRadius: 15,
-            alignSelf: "flex-end",
-            width: 89,
-            height: 27,
-          }}
-        >
-          <Button
-            onPress={() => navigation.navigate("RecViewall")}
-            title="View all"
-          ></Button>
-          <Text>View all</Text>
-        </View>
-
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={true}
-          style={{ height: 300 }}
-        >
-          <LinearGradient
-            colors={["rgba(0,164,109,0.09)", "transparent"]}
-            style={{
-              position: "absolute",
-              left: 0,
-              right: 0,
-              height: 130,
-              marginTop: 170,
-              top: 0,
-              width: 9999,
-            }}
-          />
-          <TouchableOpacity
-            // key={ind}
-            //  onPress={() => OpenInfo(val)}
-            style={{
-              height: 250,
-              elevation: 2,
-              //   backgroundColor: "#FFF",
-              marginLeft: 20,
-              marginTop: 20,
-              borderRadius: 15,
-              marginBottom: 10,
-              width: 160,
-            }}
-            //  disabled={val.deleted}
-          >
-            <Modal
-              animationType="slide"
-              transparent={true}
-              visible={modalVisible}
-              /*onRequestClose={() => {
-                Alert.alert("Modal has been closed.");
-                setModalVisible(!modalVisible);
-              }}*/
-            >
-              <View>
-                <View
-                  style={{
-                    backgroundColor: "white",
-                    margin: 50,
-                    padding: 50,
-                    marginTop: 400,
-                  }}
-                >
-                  <Text> hhhhhhhereeeeeehhhhhh write category</Text>
-                  <Icon
-                    onPress={() => setModalVisible(false)}
-                    name="close-outline"
-                    size={30}
-                    style={{ color: "black", marginLeft: -20, marginTop: -40 }}
-                  />
-                </View>
+              >
+                <Icon
+                  onPress={() => setModalVisible(false)}
+                  name="close-outline"
+                  size={30}
+                  style={{ color: "red", textAlign: "right" }}
+                />
+                <Text>Categories Are {categories.join("_-")}</Text>
               </View>
-            </Modal>
+            </View>
+          </Modal>
 
-            <View
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={true}
+            style={{ height: 500 }}
+          >
+            <LinearGradient
+              colors={["rgba(0,164,109,0.09)", "transparent"]}
               style={{
-                flexDirection: "row",
-                paddingTop: 10,
-                paddingHorizontal: 10,
+                position: "absolute",
+                left: 0,
+                right: 0,
+                height: "55%",
+                marginTop: 170,
+                top: 0,
+                width: 9999,
               }}
-            >
+            />
+            {RecommendationList.length > 0 ? (
+              RecommendationList.map((val, ind) => {
+                return (
+                  faveIds.length > 0 &&
+                  !faveIds.join("").includes(val.id) && (
+                    <TouchableOpacity
+                      key={ind}
+                      onPress={() => navigation.navigate("BookInfo", val)}
+                      style={{
+                        height: 250,
+                        elevation: 2,
+                        backgroundColor: "#FFF",
+                        marginLeft: 20,
+                        marginTop: 20,
+                        borderRadius: 15,
+                        marginBottom: 10,
+                        width: 160,
+                      }}
+                      disabled={val.deleted}
+                    >
+                      {val.deleted && (
+                        <View
+                          style={{
+                            position: "absolute",
+                            zIndex: 1,
+                            alignSelf: "center",
+                            width: 160,
+                            opacity: 0.6,
+                            justifyContent: "center",
+                            alignItems: "center",
+                            backgroundColor: "#525454",
+                            height: 250,
+                          }}
+                        >
+                          <Text
+                            style={{
+                              color: "white",
+                              textAlign: "center",
+                              fontWeight: "bold",
+                            }}
+                          >
+                            DELETED
+                          </Text>
+                        </View>
+                      )}
+
+                      <Image
+                        source={{ uri: val.poster }}
+                        style={{ width: "100%", height: 200 }}
+                      />
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          paddingTop: 10,
+                          paddingHorizontal: 10,
+                        }}
+                      >
+                        <Text
+                          style={{
+                            fontWeight: "bold",
+                          }}
+                        >
+                          {Datacat(val.title, 25)}
+                          {"\n"}
+                        </Text>
+                      </View>
+                    </TouchableOpacity>
+                  )
+                );
+              })
+            ) : (
               <Text
                 style={{
+                  marginTop: -10,
+                  marginLeft: 130,
+                  fontSize: 15,
                   fontWeight: "bold",
+                  color: "grey",
+                  alignSelf: "center",
                 }}
-              ></Text>
-            </View>
-          </TouchableOpacity>
-          <Text
-            style={{
-              //  flex: 1,
-              //textAlign: "center",
-              marginTop: -10, ////////////////////////////////////////
-              marginLeft: 10, ////////////////////////////////////////
-              fontSize: 15,
-              fontWeight: "bold",
-              color: "grey",
-              alignSelf: "center",
-            }}
-          >
-            Empty
-          </Text>
-        </ScrollView>
-      </View>
+              >
+                Empty
+              </Text>
+            )}
+          </ScrollView>
+        </View>
+      </ScrollView>
     </View>
   );
 }
--- navigation/screens/RecViewall.js
+++ navigation/screens/RecViewall.js
@@ -1,30 +1,196 @@
-import * as React from "react";
 import {
-  Image,
-  ImageBackground,
-  SafeAreaView,
-  ScrollView,
-  StyleSheet,
-  Text,
   View,
+  Text,
+  TextInput,
+  StyleSheet,
+  Button,
+  FlatList,
+  ScrollView,
+  SafeAreaView,
   TouchableOpacity,
-  Alert,
+  Image,
+  Dimensions,
+  ImageBackground,
 } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
+import { useState, useEffect } from "react";
 
-export default function RecViewall({ navigation }) {
+import Icon from "react-native-vector-icons/Ionicons";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../../config/firebase";
+
+export default function RecViewall({ navigation, route }) {
+  let books = route.params;
+  const Datacat = (str, num) => {
+    if (str.length > num) {
+      return str.substring(0, num) + "...";
+    }
+    return str;
+  };
+  let OpenInfo = async (val) => {
+    const colRef = doc(db, "Book", val.id);
+    const snapshot = await getDoc(colRef);
+    let book = snapshot.data();
+    book.id = val.id;
+    navigation.navigate("BookInfo", book);
+  };
   return (
-    <View>
-      <SafeAreaView>
-        <ScrollView>
-          <Icon
-            name="arrow-back-outline"
-            size={40}
-            style={{ color: "black", marginTop: 30, marginLeft: 10 }}
-            onPress={() => navigation.goBack()}
-          />
+    <SafeAreaView style={{ flex: 1 }}>
+      <ImageBackground
+        style={{ flex: 1 }}
+        source={require("./222.jpg")}
+        resizeMode="cover"
+      >
+        <Icon
+          name="arrow-back-outline"
+          size={40}
+          style={{ color: "black", marginTop: 30, marginLeft: 10 }}
+          onPress={() => navigation.goBack()}
+        />
+        <ScrollView showsVerticalScrollIndicator={true}>
+          <View
+            style={{
+              flex: 1,
+              flexDirection: "row",
+              flexWrap: "wrap",
+              justifyContent: "center",
+            }}
+          >
+            {books.length > 0 ? (
+              books.map((val, ind) => (
+                <TouchableOpacity
+                  key={ind}
+                  onPress={() => OpenInfo(val)}
+                  style={{
+                    height: 250,
+                    elevation: 2,
+                    backgroundColor: "#EFF3EF",
+                    marginLeft: 20,
+                    marginTop: 20,
+                    borderRadius: 15,
+                    marginBottom: 10,
+                    width: 160,
+                  }}
+                  disabled={val.deleted}
+                >
+                  {val.deleted && (
+                    <View
+                      style={{
+                        position: "absolute",
+                        zIndex: 1,
+                        alignSelf: "center",
+                        width: 160,
+                        opacity: 0.6,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        backgroundColor: "#525454",
+                        height: 250,
+                      }}
+                    >
+                      <Text
+                        style={{
+                          color: "white",
+                          textAlign: "center",
+                          fontWeight: "bold",
+                          // backgroundColor: "black",
+                        }}
+                      >
+                        DELETED
+                      </Text>
+                    </View>
+                  )}
+
+                  <Image
+                    source={{ uri: val.poster }}
+                    style={{ width: "100%", height: 200 }}
+                  />
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      paddingTop: 10,
+                      paddingHorizontal: 10,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontWeight: "bold",
+                      }}
+                    >
+                      {Datacat(val.title, 25)}
+                      {"\n"}{" "}
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              ))
+            ) : (
+              <Text
+                style={{
+                  flex: 1,
+                  textAlign: "center",
+                  marginTop: 50,
+                  fontSize: 30,
+                  fontWeight: "bold",
+                  color: "grey",
+                }}
+              >
+                Book List Is Empty
+              </Text>
+            )}
+          </View>
         </ScrollView>
-      </SafeAreaView>
-    </View>
+      </ImageBackground>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    height: "70%",
+    width: "90%",
+    borderRadius: 1,
+    resizeMode: "stretch",
+    justifyContent: "center",
+    shadowColor: "black",
+    shadowOpacity: 0.9,
+    shadowOffset: {
+      width: 2,
+      height: 8,
+    },
+    alignSelf: "center",
+
+    //alignItems: "center",
+    // margin: 15,
+  },
+  oneBook: {
+    //padding: 1,
+    height: 130,
+    justifyContent: "center",
+    width: 350,
+    backgroundColor: "lightgrey",
+    borderRadius: 25,
+    margin: 50,
+    alignItems: "center",
+  },
+  card: {
+    height: "90%",
+    backgroundColor: "#EDF5F0",
+    marginHorizontal: 10,
+    borderRadius: 10,
+    margin: 10,
+    marginBottom: 30,
+    padding: 10,
+    borderColor: "#00a46c",
+    borderWidth: 0.2,
+  },
+  categoryContainer: {
+    flexDirection: "row",
+    margin: 10,
+    justifyContent: "space-between",
+    flex: 1,
+  },
+  categoryText: { fontSize: 15, color: "grey", fontWeight: "bold" },
+  categoryTextSelected: {
+    color: "green",
+    paddingBottom: 5,
+    borderBottomWidth: 2,
+  },
+});
